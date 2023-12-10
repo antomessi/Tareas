66d30f65d95eafdd1570a00fed135d6027c72334
antop <antomessinaa@gmail.com>
2023-12-10 20:16:37 -0300

    1.8

---
+++ tp1/script2.js
@@ -0,0 +1,9 @@
+//8.-Escribe un programa que pida un número y diga si es divisible por 2
+let num = parseInt(prompt("ingrese num"));
+
+if (num % 2 === 0) {
+    alert(`${num} es divisible por dos`)
+} else {
+    alert(`${num} no es divisible por dos`)
+    
+}
